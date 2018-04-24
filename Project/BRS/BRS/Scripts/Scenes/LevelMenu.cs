dd3b644e457bab85ea5408ad25333fd261b8d833
nhuart <nicolas.huart198@gmail.com>
2018-04-24 09:20:51 +0200

    police paths

--- Project/BRS/BRS/Scripts/Scenes/LevelMenu.cs
+++ Project/BRS/BRS/Scripts/Scenes/LevelMenu.cs
@@ -6,16 +6,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace BRS.Scripts.Scenes {
     class LevelMenu : Scene {
 
         public override void Load() {
             CreateManagers();
+            //MenuScene();
+
+            // @ Simone
+            //List<List<Vector3>> policePaths = File.ReadPolicePaths("Load/PolicePaths.txt");
         }
 
         private void CreateManagers() {
-
             // @Simone make sure this doesnt get deleted
             // Has to be called before the next manager
             GameObject ScenesCommManager = new GameObject("scenesComManager");
@@ -24,7 +29,16 @@ namespace BRS.Scripts.Scenes {
 
             GameObject Manager = new GameObject("manager");
             Manager.AddComponent(new MenuManager());
+        }
 
+        void MenuScene() {
+            Material insideMat = new Material(File.Load<Texture2D>("Images/textures/polygonHeist"), File.Load<Texture2D>("Images/lightmaps/menu_inside"));
+            GameObject insideScene = new GameObject("menu_inside", File.Load<Model>("Models/scenes/menu_inside"));
+            insideScene.material = insideMat;
+
+            Material outsideMat = new Material(File.Load<Texture2D>("Images/textures/polygonCity"), File.Load<Texture2D>("Images/lightmaps/menu_outside"));
+            GameObject outsideScene = new GameObject("menu_outside", File.Load<Model>("Models/scenes/menu_outside"));
+            outsideScene.material = outsideMat;
         }
 
     }
