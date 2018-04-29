475b3a4ab68766f219f4fb5063880fe6bd5fd3e8
Andreas Emch <andreas.emch@bluewin.ch>
2018-04-29 10:42:33 +0200

    Start with dynamic shadow

---
+++ Project/BRS/BRS/Scripts/PlayerScripts/DynamicShadow.cs
@@ -0,0 +1,39 @@
+﻿// (c) Simone Guggiari 2018
+// ETHZ - GAME PROGRAMMING LAB
+
+using BRS.Engine;
+using Microsoft.Xna.Framework;
+
+namespace BRS.Scripts.PlayerScripts {
+    class DynamicShadow : Component {
+        ////////// deals with the attack of the player //////////
+
+        // --------------------- VARIABLES ---------------------
+
+        //public
+
+        //private
+
+        // const
+
+        //reference
+        Transform _target;
+
+        // --------------------- BASE METHODS ------------------
+        public override void Start() {
+            _target = gameObject.transform;
+        }
+
+        public override void Update() { }
+
+
+        // --------------------- CUSTOM METHODS ----------------
+
+
+        // queries
+
+
+        // other
+
+    }
+}
