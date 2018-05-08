9f2ad9c9543430a41f3a7f4fe351705be7433916
Andreas Emch <andreas.emch@bluewin.ch>
2018-05-08 16:44:34 +0200

    Weight falls in direction of player

--- Project/BRS/BRS/Scripts/PowerUps/Weight.cs
+++ Project/BRS/BRS/Scripts/PowerUps/Weight.cs
@@ -2,6 +2,8 @@
 // ETHZ - GAME PROGRAMMING LAB
 
 using BRS.Engine;
+using BRS.Engine.Physics;
+using BRS.Engine.Physics.RigidBodies;
 using BRS.Scripts.Managers;
 using BRS.Scripts.PlayerScripts;
 using Microsoft.Xna.Framework;
@@ -34,7 +36,13 @@ namespace BRS.Scripts.PowerUps {
             //instantiate falling weight
             Player randomEnemyPlayer = ElementManager.Instance.Enemy(Owner.TeamIndex);
             Vector3 spawnPos = randomEnemyPlayer.transform.position + Vector3.Up * WeightSpawnHeight;
-            GameObject fallingWeight = GameObject.Instantiate("fallingWeightPrefab", spawnPos, MyRandom.YRotation());
+
+            Vector3 velocity = Conversion.ToXnaVector(randomEnemyPlayer.gameObject.GetComponent<MovingRigidBody>().RigidBody.LinearVelocity);
+            velocity.Normalize();
+            velocity = 3 * velocity;
+            //velocity = 2.0f * Vector3.Down;
+
+            GameObject fallingWeight = GameObject.Instantiate("fallingWeightPrefab", spawnPos, MyRandom.YRotation(), velocity);
             ElementManager.Instance.Add(fallingWeight);
         }
     }
