29bc0faebe82f38762ff8d38ea7f270577a1c499
Andreas Emch <andreas.emch@bluewin.ch>
2018-05-21 13:13:38 +0200

    Adjustment of the DoF

--- Project/BRS/BRS/Engine/PostProcessing/PostProcessingManager.cs
+++ Project/BRS/BRS/Engine/PostProcessing/PostProcessingManager.cs
@@ -44,8 +44,8 @@ namespace BRS.Engine.PostProcessing {
         private RenderTarget2D _blurTarget2;
         private RenderTarget2D _renderTarget1;
         private RenderTarget2D _renderTarget2;
-        private const float Distance = 10f;
-        private const float Range = 40f;
+        private float Distance = 10f;
+        private float Range = 100f;
 
         #endregion
 
